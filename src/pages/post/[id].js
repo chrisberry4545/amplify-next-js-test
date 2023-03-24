c0580ffc1203466e5bc9ff5afa96fe9d1255d87c
Christopher Berry <chrisberry4545@googlemail.com>
2023-03-24 15:16:20 +0000

    getStaticProps test

--- src/pages/post/[id].js
+++ src/pages/post/[id].js
@@ -3,6 +3,14 @@ export const getStaticPaths = async () => {
     paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
     fallback: false, // can also be true or 'blocking'
   }
+};
+
+// `getStaticPaths` requires using `getStaticProps`
+export async function getStaticProps(context) {
+  return {
+    // Passed to the page component as props
+    props: { post: {} },
+  }
 }
 
 const Post = ({ post }) => {
